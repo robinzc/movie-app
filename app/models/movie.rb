a68e98237da791e276b24d2003619ecf01f6cdec
robin <robin.j.soto@gmail.com>
2020-09-16 17:47:29 -0700

    adds validations to movies and actors, adds happy/sad paths to movies and actors controllers

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ApplicationRecord
+  
+  validates :title, presence: true
+  validates :year, length: { is: 4 }
+  validates :plot, length: { in: 2..500 }
+  validates :director, presence: true
+
 end
